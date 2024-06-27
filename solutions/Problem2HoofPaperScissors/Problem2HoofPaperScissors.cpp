f0ec7e272a3e61fe28d4db11c354cbf42a222099
73anthonyL <73anthony.lu@gmail.com>
2024-06-27 14:46:27 -0700

    hoof paper scissors (gold) init

---
+++ solutions/Problem2HoofPaperScissors/Problem2HoofPaperScissors.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <array>
+#include <math.h>
+#include <numeric>
+#include <set>
+#include <cstdint>
+#include <string>
+#include <map>
+#include <bitset>
+using namespace std;
+using ll = long long;
+#define dbg(v) \
+    cout << #v << " = " << (v) << ", ";
+    
+int n;
+void solve() {
+    int a; cin >> a; cout << a;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+		
+	int t = 1;
+    cin >> t;
+	while (t--) {
+		solve();
+	}
+}
