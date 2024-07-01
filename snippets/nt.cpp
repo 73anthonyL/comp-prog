41bf1a8833ba51e163eb9e95ae67bca110caf696
73anthonyL <73anthony.lu@gmail.com>
2024-07-01 10:25:53 -0700

    added prime factorization and check prime functions

---
+++ snippets/nt.cpp
@@ -0,0 +1,24 @@
+// prime factorization algorithm
+vector<int> factors(int n) {
+    vector<int> f;
+    for (int x = 2; x*x <= n; x++) {
+        while (n%x == 0) {
+            f.push_back(x);
+            n /= x;
+        }
+    }
+
+    if (n > 1) f.push_back(n);
+    return f;
+}
+
+// check if prime
+bool prime(int n) {
+    if (n < 2) return false;
+    for (int x = 2; x*x <= n; x++) {
+        if (n%x == 0) return false;
+    }
+
+    return true;
+}
+
