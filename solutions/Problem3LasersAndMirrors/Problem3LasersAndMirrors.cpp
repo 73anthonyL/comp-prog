a25bfd1cbb49211801b0fde177a4c1f59b53539c
73anthonyL <73anthony.lu@gmail.com>
2024-07-07 14:08:15 -0700

    finished lasers and mirrors

--- solutions/Problem3LasersAndMirrors/Problem3LasersAndMirrors.cpp
+++ solutions/Problem3LasersAndMirrors/Problem3LasersAndMirrors.cpp
@@ -24,70 +24,48 @@ using pii = pair<int, int>;
 #define mp make_pair
 
 int n, xl, yl, xb, yb;
-map<pii, int> m;
-map<int, vector<int>> xLevel, yLevel;
+unordered_map<int, vector<int>> xLevel, yLevel;
+set<pii> vis; // first indicates val, second indicates x or a y (x = 0, y = 1)
+queue<array<int, 3>> q; // bfs queue {val, dist, type}
 void solve() {
     cin >> n;
-    vector<vector<int>> adj(n+3, vector<int>());
-    int ct = 1;
+    pii beam, destination;
     for (int i = 0; i < n+2; i++) {
         int x, y; cin >> x >> y;
-        m[mp(x, y)] = ct++;
-        if (xLevel.find(x) == xLevel.end()) {
-            xLevel[x] = {ct-1};
-        }
-        else {
-            xLevel[x].emplace_back(ct-1);
-        }
-
-        if (yLevel.find(y) == yLevel.end()) {
-            yLevel[y] = {ct-1};
-        }
-        else {
-            yLevel[y].emplace_back(ct-1);
-        }
+        if (i == 0) beam = mp(x, y);
+        if (i == 1) destination = mp(x, y);
+        xLevel[x].emplace_back(y);
+        yLevel[y].emplace_back(x);
     }
 
-    for (auto [k, v] : xLevel) {
-        if (v.size() == 1) continue;
-        for (int i = 0; i < v.size(); i++) {
-            for (int j = i+1; j < v.size(); j++) {
-                adj[v[i]].emplace_back(v[j]);
-                adj[v[j]].emplace_back(v[i]);
+    q.push({beam.first, 0, 0});
+    q.push({beam.second, 0, 1});
+
+    while (!q.empty()) {
+        auto [val, dist, isY] = q.front(); q.pop();
+        if (isY && val == destination.second || !isY && val == destination.first) {
+            cout << dist << "\n";
+            return;
+        }
+
+        vis.insert(mp(val, isY));
+        if (isY) {
+            for (auto x : yLevel[val]) {
+                if (vis.find(mp(x, !isY)) == vis.end()) {
+                    q.push({x, dist+1, !isY});
+                }
             }
         }
-    }
-
-    for (auto [k, v] : yLevel) {
-        if (v.size() == 1) continue;
-        for (int i = 0; i < v.size(); i++) {
-            for (int j = i+1; j < v.size(); j++) {
-                adj[v[i]].emplace_back(v[j]);
-                adj[v[j]].emplace_back(v[i]);
+        else {
+            for (auto y : xLevel[val]) {
+                if (vis.find(mp(y, !isY)) == vis.end()) {
+                    q.push({y, dist+1, !isY});
+                }
             }
         }
     }
     
-    vector<bool> vis(n+3);
-    vector<int> dist(n+3, INT32_MAX);
-    queue<int> q; q.push(1); dist[1] = 0;
-    while (!q.empty()) {
-        int v = q.front(); q.pop();
-        vis[v] = true;
-        for (auto x : adj[v]) {
-            if (!vis[x]) {
-                dist[x] = dist[v]+1; 
-                q.push(x);
-            }
-        }
-    }
-
-    if (dist[2] == INT32_MAX) {
-        cout << -1 << "\n";
-    }
-    else {
-        cout << dist[2]-1 << "\n";
-    }
+    cout << - 1 << "\n";
 }
 
 int main() {
