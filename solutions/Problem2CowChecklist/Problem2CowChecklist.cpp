fa09e4b14e71342b25e90c33620e8554390cfce1
73anthonyL <73anthony.lu@gmail.com>
2024-07-07 17:48:59 -0700

    finished solving cow checklist

--- solutions/Problem2CowChecklist/Problem2CowChecklist.cpp
+++ solutions/Problem2CowChecklist/Problem2CowChecklist.cpp
@@ -13,6 +13,7 @@
 #include <unordered_map>
 #include <bitset>
 #include <iomanip>
+#include <cassert>
 
 using namespace std;
 using ll = long long;
@@ -25,13 +26,13 @@ using pll = pair<ll, ll>;
 #define mp make_pair
 
 const int MAXH = 1001;
-const ll INF = 10000000000000;
+const ll INF = (1L<<60);
 
 ll dp[MAXH][MAXH][2];
 pll holsteins[MAXH], guernseys[MAXH];
 
 ll dist (pll p1, pll p2) {
-    ll ret = (abs(p1.first-p2.first) * abs(p1.first-p2.first)) + (abs(p1.second-p2.second) * abs(p1.second-p2.second));
+    ll ret = ((p1.first-p2.first) * (p1.first-p2.first)) + ((p1.second-p2.second) * (p1.second-p2.second));
     return ret;
 }
 
@@ -48,21 +49,27 @@ void solve() {
         guernseys[i] = mp(x, y);
     }
     
-    for (int i = 1; i <= g; i++) {
-        dp[0][i][0] = dp[0][i][1] = INF;
-        dp[i][0][1] = INF;
+    for (int i = 0; i <= h; i++) {
+        for (int j = 0; j <= g; j++) {
+            dp[i][j][0] = dp[i][j][1] = INF;
+        }
     }
-    dp[1][1][0] = INF;
-    dp[1][1][1] = dist(holsteins[1], guernseys[1]);
-    for (int hdone = 1; hdone <= h; hdone++) {
-        for (int gdone = 1; gdone <= g; gdone++) {
-            // 0 means that @h cow
-            dp[hdone][gdone][0] = min(
-                    dp[hdone-1][gdone][0]+dist(holsteins[hdone-1], holsteins[hdone]),
-                    dp[hdone-1][gdone][1]+dist(guernseys[gdone], holsteins[hdone]));
-            dp[hdone][gdone][1] = min(
-                    dp[hdone][gdone-1][0]+dist(holsteins[hdone], guernseys[gdone]),
-                    dp[hdone][gdone-1][1]+dist(guernseys[gdone-1], guernseys[gdone]));
+    dp[1][0][0] = 0;
+
+    for (int hdone = 0; hdone <= h; hdone++) {
+        for (int gdone = 0; gdone <= g; gdone++) {
+            for (int endG = 0; endG < 2; endG++) {
+                if ((!endG && !hdone) || (endG && !gdone)) continue;
+                pll source = ((endG) ? guernseys[gdone] : holsteins[hdone]);
+
+                if (gdone < g) {
+                    dp[hdone][gdone+1][1] = min(dp[hdone][gdone+1][1], dist(source, guernseys[gdone+1])+dp[hdone][gdone][endG]);
+                }
+
+                if (hdone < h) {
+                    dp[hdone+1][gdone][0] = min(dp[hdone+1][gdone][0], dist(source, holsteins[hdone+1])+dp[hdone][gdone][endG]);
+                }
+            }
         }
     }
     
