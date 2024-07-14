73bfe671e93847bdc0751ead79124b58ef5495b0
73anthonyL <73anthony.lu@gmail.com>
2024-07-14 07:38:31 -0700

    finished Why did the cow cross the road

--- solutions/Problem1WhyDidTheCowCrossTheRoad/Problem1WhyDidTheCowCrossTheRoad.cpp
+++ solutions/Problem1WhyDidTheCowCrossTheRoad/Problem1WhyDidTheCowCrossTheRoad.cpp
@@ -19,68 +19,69 @@ using ll = long long;
 
 #define dbg(v) \
     cerr << #v << " = " << (v) << "\n";
-#define _GLIBCXX_DEBUG
-
+#define int long long 
 #define mp make_pair
 
 const int MAXN = 100;
+const int INF = INT32_MAX;
+
 int g[MAXN][MAXN];
-int dp[MAXN][MAXN][3] = {-1};
-queue<array<int, 3>> q;
+int dist[MAXN][MAXN];
+bool proc[MAXN][MAXN];
+priority_queue<array<int, 3>> q;
 
-int dr[] = {0, 0, -1, 1};
-int dc[] = {-1, 1, 0, 0};
+int dr[] = {3, -3, 2, -2, 1, -1, 0, 2, -2, -1, 1, 0, 0, 0, 1, -1};
+int dc[] = {0, 0, 1, 1, 2, 2, 3, -1, -1, -2, -2, -3, -1, 1, 0, 0};
 
-int prev(int &order) {
-    return (order-1+3)%3;
-}
-
-int n, t; 
+int n, T; 
 bool ok(int r, int c) {
     return r < n && c < n && r >= 0 && c >= 0;
 }
 
-int minTimeToState(int r, int c, int order) {
-    if (dp[r][c][order] != -1) {
-        // this state has been cached already
-        return dp[r][c][order];
-    }
-
-    int minNeighbor = INT32_MAX;
-    for (int i = 0; i < 4; i++) {
-        if (ok(r+dr[i], c+dc[i])) {
-            minNeighbor = min(minNeighbor, minTimeToState(r+dr[i], c+dc[i], prev(order)));
-        }
-    }
-    
-    dp[r][c][order] = (minNeighbor + ((order == 2) ? g[r][c] : 0) + t);
-    return dp[r][c][order];
+int manDist(int r1, int c1, int r2, int c2) {
+    int ret = abs(r1-r2)+abs(c1-c2);
+    return ret;
 }
 
 void solve() {
-    cin >> n >> t;
+    cin >> n >> T;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cin >> g[i][j];
+            dist[i][j] = INF;
         }
     }
+    
+    q.push({0, 0, 0});
+    while(!q.empty()) {
+        auto [val, r, c] = q.top(); q.pop();
+        if (proc[r][c]) continue;
+        proc[r][c] = true;
+        if (manDist(r, c, n-1, n-1) < 3) {
+            if (-val+(T*manDist(r, c, n-1, n-1)) < dist[n-1][n-1]) {
+                dist[n-1][n-1] = -val+(T*manDist(r, c, n-1, n-1));
+            }
+            continue;
+        }
 
-    dp[0][0][0] = 0; 
-    cout << min(min(minTimeToState(n-1, n-1, 2), minTimeToState(n-1, n-1, 1)), minTimeToState(n-1, n-1, 0)) << "\n";
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            for (int k = 0; k < 3; k++) {
-                dbg(dp[i][j][k]);
+        for (int i = 0; i < 16; i++) {
+            if (ok(r+dr[i], c+dc[i]) && -val+(3*T)+g[r+dr[i]][c+dc[i]] < dist[r+dr[i]][c+dc[i]]) {
+                dist[r+dr[i]][c+dc[i]] = -val+(3*T)+g[r+dr[i]][c+dc[i]];
+                q.push({-dist[r+dr[i]][c+dc[i]], r+dr[i], c+dc[i]});
             }
         }
     }
+
+    cout << dist[n-1][n-1] << "\n";
 }
 
-int main() {
+signed main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
-		
+	
+    freopen("visitfj.in", "r", stdin);
+    freopen("visitfj.out", "w", stdout);
+
 	int t = 1;
 	while (t--) {
 		solve();
