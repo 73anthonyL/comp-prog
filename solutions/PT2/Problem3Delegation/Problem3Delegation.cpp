6f86da9f9a77237ae0c2207501957733682ff1da
73anthonyL <73anthony.lu@gmail.com>
2024-08-06 07:31:35 -0700

    dk change

---
+++ solutions/PT2/Problem3Delegation/Problem3Delegation.cpp
@@ -0,0 +1,37 @@
+#define _GLIBCXX_DEBUG
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cmath>
+#include <cstdint>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+#define dbg(v) cerr << #v << " = " << (v) << "\n";
+
+#define mp make_pair
+
+int n;
+void solve() { cin >> n; }
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
