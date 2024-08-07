d9d9a73b5ad8180a931e4ae19d3abdcbac09e620
73anthonyL <73anthony.lu@gmail.com>
2024-08-06 23:41:43 -0700

    template changed

--- snippets/template.cpp
+++ snippets/template.cpp
@@ -1,19 +1,5 @@
 #define _GLIBCXX_DEBUG
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <queue>
-#include <array>
-#include <cmath>
-#include <numeric>
-#include <set>
-#include <unordered_set>
-#include <cstdint>
-#include <string>
-#include <map>
-#include <unordered_map>
-#include <bitset>
-#include <iomanip>
+#include <bits/stdc++.h>
 
 using namespace std;
 using ll = long long;
